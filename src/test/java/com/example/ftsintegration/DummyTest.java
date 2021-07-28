18ddd6f21dcb6fa037d2465d36509e42c6088327
Dmytro <dmytro.polishchuk@auto1.com>
2021-07-28 14:40:27 +0300

    first commit

---
+++ src/test/java/com/example/ftsintegration/DummyTest.java
@@ -0,0 +1,12 @@
+package com.example.ftsintegration;
+
+import org.junit.jupiter.api.Test;
+
+
+public class DummyTest {
+
+    @Test
+    public void test() {
+        System.out.println("Dummy test success!");
+    }
+}
